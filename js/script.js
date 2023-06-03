1f499d27dc2a9739be52c2db105b23a93cf85021
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-06-02 22:20:57 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,24 @@ const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext('2d');
 
 canvas.addEventListener('mousemove',mousemoved);
+canvas.addEventListener('mousedown',mousedown);
+window.addEventListener('mouseup',mouseup);
 
+var mouseIsDown = false;
 function mousemoved(e)
 {
- 
+  if(mouseIsDown)
+      console.log("mouse moved");
+}
+
+function mousedown(e)
+{
+  mouseIsDown = true;
+   console.log("mouse down");
+}
+
+function mouseup(e)
+{
+  mouseIsDown = false;
+   console.log("mouse up");
 }
