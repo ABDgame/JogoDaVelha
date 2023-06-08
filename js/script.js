c0790a06cb3e81b2d0eacc31c68261749e4e737b
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-06-07 22:28:45 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,16 +13,28 @@ window.addEventListener('mouseup',mouseup);
 var mouseIsDown = false;
 var imageAdded = false;
 var mouseDownPos = {x:0,y:0};
+var mainImage = null;
+var tempBox = {};
 
 function mousemoved(e)
 {
   if(mouseIsDown){  
-     //cropped_images_div.innerHTML = 'x:${e.clientX - canvas.offsetLeft} y:${e.clientY - canvas.offsetTop}';
+     if(!imageAdded)
+     {
+         alert("Adicione uma imagem primeiro!!!")
+         return;
+      }
      ctx.clearReact(0,0,canvas.width,canvas.height);
+     redraw();
      let width = (e.clientX - canvas.offsetleft) - mouseDownPos.x;
      let height = (e.clientY - canvas.offsetTop) - mouseDownPos.y;
-     ctx.fillStyle = "grey";
-     ctx.fillRect(mouseDownPos.x,mouseDownPos.y,width,height);
+     ctx.strokeStyle = "grey";
+     ctx.strokeRect(mouseDownPos.x,mouseDownPos.y,width,height);
+     //save temp box params
+     tempBox.x = mouseDownPos.x;
+     tempBox.y = mouseDownPos.y;
+     tempBox.width = width;
+     tempBox.height = height;
      
   }
 }
@@ -51,5 +63,14 @@ function addImage(file)
      imageAdded = true;
      let height = img.naturalHeigth / img.naturalWidth;
      ctx.drawImage(img,0,0, canvas.width, height * canvas.width);
+     mainImage = img;
+  }
+}
+function redraw()
+{
+  if(mainImage)
+  {
+     let height=img.naturalHeight/img.naturalWidth;
+     ctx.drawImage(img,0,0,canvas.width,height * canvas.width);
   }
 }
