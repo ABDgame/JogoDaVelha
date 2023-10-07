8dfc5dd6aed012fb9bf539fe645c0adb60f2a47f
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-10-06 22:28:20 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-
+let result = document.getElementById('result');
+let resetBtn = document.getElementById('result');
+let boxes = document.getElementByClassName('box');
+let arrBoxes = Array.from(boxes);
