8906b90c0f61c30322efb4711e6a03de1440b8cb
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-10-07 16:44:39 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,14 +9,45 @@ const player2 = "O";
 let currentPlayer = player1;
 let spaces = Array(9).fill(null);
 
-const onGameaStart = () => {
+const onGameStart = ()=> {
   arrBoxes.forEach(box=> box.addEventListener('click', (e)=>{
       const id = e.target.id;
       if(!spaces[id]){   
           spaces[id] = currentPlayer;
           e.target.innerText = currentPlayer;
+          if(playerHasWon() !== false){
+              result.innerText = 
+          }
+            
+          if(currentPlayer == "X"){
+              currentPlayer = "O";
+          }
+          else{
+              currentPlayer = "X";
+          }
       }
   }))
 }
 
+const winningCombos = [
+   [0,1,2],
+   [3,4,5],
+   [6,7,8],
+   [0,3,6],
+   [1,4,7],
+   [2,5,8],
+   [0,4,8],
+   [2,4,6]
+]
+
+const playerHasWon = ()=>{
+    for (const condition of winningCombos){
+         let [a,b,c] = condition;
+
+         if(spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c])
+            return [a,b,c];
+         }
+    }
+    return false;
+}
 onGameStart()
