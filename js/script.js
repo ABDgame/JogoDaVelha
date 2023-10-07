008b3efe686a38f6cee0849c34f8562bbf189744
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-10-07 16:05:28 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@ let result = document.getElementById('result');
 let resetBtn = document.getElementById('result');
 let boxes = document.getElementByClassName('box');
 let arrBoxes = Array.from(boxes);
+
+
+const player1 = "X";
+const player2 = "O";
+let currentPlayer = player1;
+let spaces = Array(9).fill(null);
+
+const onGameaStart = () => {
+  arrBoxes.forEach(box=> box.addEventListener('click', (e)=>{
+      const id = e.target.id;
+      if(!spaces[id]){   
+          spaces[id] = currentPlayer;
+          e.target.innerText = currentPlayer;
+      }
+  }))
+}
+
+onGameStart()
