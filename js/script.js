ee181dea934464ce16c0f4c4f23d8237630e3536
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-10-08 13:23:38 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let result = document.getElementById('result');
-let resetBtn = document.getElementById('result');
-let boxes = document.getElementByClassName('box');
+let resetBtn = document.getElementById('reset');
+let boxes = document.getElementsByClassName('box');
 let arrBoxes = Array.from(boxes);
 
 
@@ -9,49 +9,49 @@ const player2 = "O";
 let currentPlayer = player1;
 let spaces = Array(9).fill(null);
 
-const onGameStart = ()=> {
-  arrBoxes.forEach(box=> box.addEventListener('click', (e)=>{
-      const id = e.target.id;
-      if(!spaces[id]){   
-          spaces[id] = currentPlayer;
-          e.target.innerText = currentPlayer;
-          if(playerHasWon() !== false){
-              result.innerText = `${currentPlayer} VENCEU!`;
-              resetBtn.innerText = "Replay";
-              let winning_boxes = playerHasWon();
-              winning_boxes.map(box => arrBoxes[box].style.backgroundColor = "#061941");
-              winning_boxes.map(box => arrBoxes[box].style.color = "yellow");
-         
-          }
-            
-          if(currentPlayer == "X"){
-              currentPlayer = "O";
-          }
-          else{
-              currentPlayer = "X";
-          }
-      }
-  }))
+const onGameStart = ()=>{
+    arrBoxes.forEach(box=> box.addEventListener('click', (e)=>{
+        const id = e.target.id;
+        if(!spaces[id]){
+            spaces[id] = currentPlayer;
+            e.target.innerText = currentPlayer;
+            if(playerHasWon() !== false){
+                result.innerText = `${currentPlayer} has Won!`;
+                resetBtn.innerText = "Replay";
+                let winning_boxes = playerHasWon();
+                winning_boxes.map(box => arrBoxes[box].style.backgroundColor = "#061941");
+                winning_boxes.map(box => arrBoxes[box].style.color = "yellow");
+
+            }
+
+            if(currentPlayer == "X"){
+                currentPlayer = "O";
+            }
+            else{
+                currentPlayer = "X";
+            }
+        }
+    }))
 }
 
 const winningCombos = [
-   [0,1,2],
-   [3,4,5],
-   [6,7,8],
-   [0,3,6],
-   [1,4,7],
-   [2,5,8],
-   [0,4,8],
-   [2,4,6]
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
 ]
 
 const playerHasWon = ()=>{
     for (const condition of winningCombos){
-         let [a,b,c] = condition;
+        let [a,b,c] = condition;
 
-         if(spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c])
+        if(spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c]){
             return [a,b,c];
-         }
+        }
     }
     return false;
 }
@@ -64,8 +64,9 @@ resetBtn.addEventListener('click', ()=>{
         box.style.backgroundColor = "";
         box.style.color = "white";
     });
-currentPlayer = player1;
-  result.innerText = "";
-  resetBtn.innerText = "Reset";
+
+    currentPlayer = player1;
+    result.innerText = "";
+    resetBtn.innerText = "Reset";
 })
 onGameStart()
